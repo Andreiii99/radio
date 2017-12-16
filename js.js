a7d670c07ceabbb0d2c987cbbdac7e3a0dcc7a6d
Andreiii99 <tofandima111111@mail.ru>
2017-12-16 12:18:50 +0200

    Create js.js

---
+++ js.js
@@ -0,0 +1,28 @@
+<!-- Yandex.Metrika counter -->
+ (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter47037711 = new Ya.Metrika({
+                    id:47037711,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true,
+                    ut:"noindex"
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+<noscript><div><img src="https://mc.yandex.ru/watch/47037711?ut=noindex" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
