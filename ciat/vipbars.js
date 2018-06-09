03a869e50214c635046841813118419a7988ad3c
Andreiii99 <tofandima111111@mail.ru>
2018-06-09 17:50:32 +0300

    Update vipbars.js

--- ciat/vipbars.js
+++ ciat/vipbars.js
@@ -1,5 +1,5 @@
 (volnorez_codes = window.volnorez_codes || (function(){
  var s=document.createElement("script");s.type="text/javascript";s.async="true";
  s.src="//volnorez.com/plugins/jscode/player_js";document.body.appendChild(s);
- return [];})())['001b7ca7']={autoplay:true,share:"local",skin:"default",style:{'bg_clr':'15,15,15','icon_clr':'199,199,199','br':'0','bw':'0','ch':'28'}};
+ return [];})())['001a0b4b'']={autoplay:true,share:"local",skin:"default",style:{'bg_clr':'15,15,15','icon_clr':'199,199,199','br':'0','bw':'0','ch':'28'}};
  if (window.VzPlayerCode) VzPlayerCode.Load('001a0b4b');
