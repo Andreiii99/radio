d5f2991dfb9078038bf997dba20234d348a583e2
Andreiii99 <tofandima111111@mail.ru>
2018-12-21 21:30:01 +0200

    Create vlechenie-radio.js

---
+++ ciat/vlechenie-radio.js
@@ -0,0 +1,5 @@
+(volnorez_codes = window.volnorez_codes || (function(){
+ var s=document.createElement("script");s.type="text/javascript";s.async="true";
+ s.src="//volnorez.com/plugins/jscode/player_js";document.body.appendChild(s);
+ return [];})())['00189dd9']={autoplay:true,share:"local",skin:"default",style:{'bg_clr':'15,15,15','icon_clr':'199,199,199','br':'0','bw':'0','ch':'28'}};
+ if (window.VzPlayerCode) VzPlayerCode.Load('00189dd9');
