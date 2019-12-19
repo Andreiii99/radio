d5b6c742e5b10b748adb110f901dbcfcc8571c50
Andreiii99 <tofandima111111@mail.ru>
2019-12-20 01:07:08 +0200

    Create secret-fm.js

---
+++ ciat/secret-fm/secret-fm.js
@@ -0,0 +1,6 @@
+(volnorez_codes = window.volnorez_codes || (function()
+ var s=document.createElement("script");s.type="text/javascript";s.async="true";
+ s.src="//volnorez.com/plugins/jscode/player_js";document.body.appendChild(s);
+ return [];})())['001eb9cb']={share:"source",skin:"skin1",style:{'bg_clr':'235,18,18','b_clr':'73,235,97','icon_clr':'0,0,0','br':'3','txt_clr':'46,217,37'}};
+ if (window.VzPlayerCode) VzPlayerCode.Load('001eb9cb'); 
+ 
