979c10d5c499c177cd23100e8d41f58cfe8c3ace
Andreiii99 <tofandima111111@mail.ru>
2019-12-22 21:31:04 +0200

    Update secret-fm.js

--- ciat/secret-fm/secret-fm.js
+++ ciat/secret-fm/secret-fm.js
@@ -1,6 +1,5 @@
-(volnorez_codes = window.volnorez_codes || (function()
+(volnorez_codes = window.volnorez_codes || (function(){
  var s=document.createElement("script");s.type="text/javascript";s.async="true";
  s.src="//volnorez.com/plugins/jscode/player_js";document.body.appendChild(s);
- return [];})())['001eb9cb']={share:"source",skin:"skin1",style:{'bg_clr':'235,18,18','b_clr':'73,235,97','icon_clr':'0,0,0','br':'3','txt_clr':'46,217,37'}};
- if (window.VzPlayerCode) VzPlayerCode.Load('001eb9cb'); 
- 
+ return [];})())['001eb9cb']={share:"local",skin:"default",style:{'bg_clr':'15,15,15','icon_clr':'199,199,199','br':'0','bw':'0','ch':'28'}};
+ if (window.VzPlayerCode) VzPlayerCode.Load('001eb9cb');
